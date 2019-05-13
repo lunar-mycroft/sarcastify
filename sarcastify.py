ce04e0bc2a8cddfa77862309ce776784e79a14dc
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2019-05-13 17:04:41 -0400

    Main basic logic working

---
+++ sarcastify.py
@@ -0,0 +1,22 @@
+from typing import Iterator
+import click
+import sys
+
+from looksRandom import looksRandom
+
+def sarcastify(string : str) -> Iterator[str]:
+    rng : Iterator[bool] = looksRandom(bias = 0.25)
+    yield string[0]
+    for char in string[1:]:
+        if not char.isalpha():
+            yield char
+            continue
+        yield char.upper() if next(rng) else char.lower()
+    
+        
+
+def main(string: str):
+    print("".join(sarcastify(string)))
+
+if __name__ == "__main__":
+    main(' '.join(sys.argv[1:]))
